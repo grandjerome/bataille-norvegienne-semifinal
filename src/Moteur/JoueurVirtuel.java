e3ceef1612bac758653747324d07930cf123ec26
naooo <nao3@hotmail.fr>
2014-12-19 20:09:58 +0100

    truc final ne modifie rien stp

--- src/Moteur/JoueurVirtuel.java
+++ src/Moteur/JoueurVirtuel.java
@@ -7,60 +7,86 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+public class JoueurVirtuel extends Joueur {
 
-public class JoueurVirtuel extends Joueur{
+	/*
+	 * (non-javadoc)
+	 */
 
-	
-	
-	public String[] nom={"Roger","Patrick","Corentin","Emilie","Albert","Alberta","Alberte","Albertine","Albin","Alda","Aldo","Aldegonde","Al�the","Alex","Alexandra"};//,Alexandre,Alexandrine,Alexia,Alexiane,Alexis,Aleyde,Alfred,Alfreda,Alice,Alida,Aline,Alix,Alo�s,Aloysius,Alphonse,Alphonsine,Ama�l,Amance,Amand,Amandine,Amoury,Ambroise,Am�d�e,Am�lie,Amos,Ana�s,Anastase,Anastasie,Anatole,Andoche,Andr�,Andr�e,Ange,Ang�le,Ang�line,Ang�lique,Anicet,Anita,Anna,Annabelle,Anne,Annette,Annick,Annie,Annonciade,Anouchka,Anouck,Anselme,Anthelme,Anthony,Antoine,Antoinette,Antonin,Apollinaire,Apolline,Apollos,Arabelle,Arcadius,Arcady,Archibald,Ariane,Arielle,Aristide,Arlette,Armand,Armande,Armel,Armelle,Arnaud,Arnold,Arnould,Ars�ne,Arthur,Astrid,Athanase,Auberi,Aubert,Aubin,Aude,Audrey,Augusta,Auguste,Augustin,Augustine,Aure,Aur�le,Aur�lia,Aur�lie,Aur�lien,Aurore,Ava,Avit,Axel,Axelle,Aymar,Aymeric,Aymone,Babette,Babine,Babita,Balbine,Balthazar,Baptiste,Barbara,Barbe,Barberine,Barnab�,Barnard,Barth�l�my,Bartolom�,Basile,Bastien,Bastienne,Bathilde,Baudouin,B�atrice,Beatrix,B�n�dicte,Benjamin,Benjamine,Beno�t,Beno�te,B�renger};
-	static private int indiceNom=0;
-	
-	
+	public String[] nom = { "Roger", "Patrick", "Corentin", "Emilie", "Albert",
+			"Alberta", "Alberte", "Albertine", "Albin", "Alda", "Aldo",
+			"Aldegonde", "Alèthe", "Alex", "Alexandra" };// ,Alexandre,Alexandrine,Alexia,Alexiane,Alexis,Aleyde,Alfred,Alfreda,Alice,Alida,Aline,Alix,Aloïs,Aloysius,Alphonse,Alphonsine,Amaël,Amance,Amand,Amandine,Amoury,Ambroise,Amédée,Amélie,Amos,Anaïs,Anastase,Anastasie,Anatole,Andoche,André,Andrée,Ange,Angèle,Angéline,Angélique,Anicet,Anita,Anna,Annabelle,Anne,Annette,Annick,Annie,Annonciade,Anouchka,Anouck,Anselme,Anthelme,Anthony,Antoine,Antoinette,Antonin,Apollinaire,Apolline,Apollos,Arabelle,Arcadius,Arcady,Archibald,Ariane,Arielle,Aristide,Arlette,Armand,Armande,Armel,Armelle,Arnaud,Arnold,Arnould,Arsène,Arthur,Astrid,Athanase,Auberi,Aubert,Aubin,Aude,Audrey,Augusta,Auguste,Augustin,Augustine,Aure,Aurèle,Aurélia,Aurélie,Aurélien,Aurore,Ava,Avit,Axel,Axelle,Aymar,Aymeric,Aymone,Babette,Babine,Babita,Balbine,Balthazar,Baptiste,Barbara,Barbe,Barberine,Barnabé,Barnard,Barthélémy,Bartolomé,Basile,Bastien,Bastienne,Bathilde,Baudouin,Béatrice,Beatrix,Bénédicte,Benjamin,Benjamine,Benoît,Benoîte,Bérenger};
+	static private int indiceNom = 0;
+
+
 	private Strategie strategie;
 
-	
-	
-	public JoueurVirtuel(){
-		if (indiceNom == 15){
-			indiceNom=0;
+	public JoueurVirtuel() {
+		if (indiceNom == 15) {
+			indiceNom = 0;
 		}
-		super.nomJoueur=nom[indiceNom];
+		super.nomJoueur = nom[indiceNom];
 		indiceNom++;
-		this.strategie = new StrategieLeMeilleurPourLaFin();	
-	}
-	public void echangerCarte() {
-		
-	}
-	
-	
-	
-	
-	
-	
-	public int determinernbCartesJouables(ArrayList<Carte> main){
-		int nbCartesJouables=0;
-		ListIterator<Carte> it = super.main.listIterator();
-		while (it.hasNext()){
-			Carte element = it.next();
-			if (element.determinerCarteJouable()){
-				nbCartesJouables++;
-			}			
+		int random = (int) (Math.random() * 2) + 1;
+		if (random == 1) {
+			this.strategie = new StrategieAleatoire();
+		} else {
+			this.strategie = new StrategieLeMeilleurPourLaFin();
 		}
-		//System.out.println("l'ordi peut jouer "+nbCartesJouables+" cartes");
+	}
+
+	public void echangerCarte() {
+		strategie.echangerCarte(this);
+	}
+
+	public int determinernbCartesJouables(ArrayList<Carte> main) {
+		int nbCartesJouables = 0;
+		ListIterator<Carte> it = main.listIterator();
+		while (it.hasNext()) {
+			Carte element = it.next();
+			if (element.determinerCarteJouable()) {
+				nbCartesJouables++;
+			}
+		}
 		return nbCartesJouables;
 	}
-	
-	
 
-	
+	/**
+	 * Getter of the property <tt>nbCartes</tt>
+	 * 
+	 * @return Returns the nbCartes.
+	 * 
+	 */
 
-	public void jouerCarte(){
+	/**
+	 * Setter of the property <tt>nbCartes</tt>
+	 * 
+	 * @param nbCartes
+	 *            The nbCartes to set.
+	 * 
+	 */
+
+	/**
+	 * Getter of the property <tt>main</tt>
+	 * 
+	 * @return Returns the main.
+	 * 
+	 */
+
+	/**
+	 * Setter of the property <tt>main</tt>
+	 * 
+	 * @param main
+	 *            The main to set.
+	 * 
+	 */
+	public void jouerCarte() {
 		strategie.poserCarteStrategique(this);
-		
+
 	}
-	public String toString(){
-		return("Joueur Virtuel "+nomJoueur+" ");
+
+	public String toString() {
+		return ("Joueur Virtuel " + nomJoueur + " ");
 	}
-	
 
 }
