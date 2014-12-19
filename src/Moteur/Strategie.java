8c3dc356244cb3c3f406ceeaca1dd21b0f8221bb
naooo <nao3@hotmail.fr>
2014-12-19 20:05:39 +0100

    truc final ne modifie rien stp

--- src/Moteur/Strategie.java
+++ src/Moteur/Strategie.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public interface Strategie {
 
 	public abstract void poserCarteStrategique(JoueurVirtuel j);
-	public abstract void echangerCartes();
-	public abstract void echangerCartes(JoueurVirtuel j);
+	public abstract void echangerCarte(JoueurVirtuel j);
 	public abstract void piocher(int nbCartesPosees, Joueur j);
 
 }
